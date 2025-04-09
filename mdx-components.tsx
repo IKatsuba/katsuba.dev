50864cb4477529fca7d81caeead188cd076d16b3
IKatsuba <igor@katsuba.dev>
2025-04-09 16:32:11 +0300

    Add Prettier configuration and streamline MDX component formatting
    - Introduced a new Prettier configuration file to enforce consistent code formatting across the project, including specific options for Markdown files.
    - Removed the old Prettier configuration file to consolidate formatting settings.
    - Simplified the return statement in the MDX components file for improved readability and consistency.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,8 +2,5 @@ import Image, { type ImageProps } from 'next/image';
 import { type MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents) {
-  return {
-    ...components,
-    Image: (props: ImageProps) => <Image {...props} />,
-  };
+  return { ...components, Image: (props: ImageProps) => <Image {...props} /> };
 }
